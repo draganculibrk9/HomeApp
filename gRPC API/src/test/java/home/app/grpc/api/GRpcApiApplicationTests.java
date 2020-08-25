28a01c8bc413066c204d72aadd0f4a49c6952fcc
Dragan Ćulibrk <draganculibrk9@gmail.com>
2020-08-25 22:07:11 +0200

    Migrate to Maven

---
+++ gRPC API/src/test/java/home/app/grpc/api/GRpcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package home.app.grpc.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GRpcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
