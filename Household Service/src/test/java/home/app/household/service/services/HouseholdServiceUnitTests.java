db2e5c4ce1f2d2709e5f06bfa8b1f00f89779f16
Dragan Ćulibrk <draganculibrk9@gmail.com>
2020-09-14 21:12:47 +0200

    Finish household service unit tests

--- Household Service/src/test/java/home/app/household/service/services/HouseholdServiceUnitTests.java
+++ Household Service/src/test/java/home/app/household/service/services/HouseholdServiceUnitTests.java
@@ -14,6 +14,7 @@ import io.grpc.StatusRuntimeException;
 import io.grpc.internal.testing.StreamRecorder;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -23,11 +24,12 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.util.AssertionErrors.fail;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
@@ -372,5 +374,316 @@ public class HouseholdServiceUnitTests {
 
         assertNotNull(message);
         assertEquals(transactionMessage, message);
+    }
+
+    @Test
+    public void editTransaction_transactionDoesNotExist_NotFoundStatusReturned() throws Exception {
+        long id = 0L;
+
+        TransactionMessage transactionMessage = TransactionMessage.newBuilder()
+                .setName("Income")
+                .setId(id)
+                .setDate(new Date().getTime())
+                .setAmount(100.0)
+                .setTransactionType(TransactionMessage.TransactionType.INCOME)
+                .build();
+
+        CreateOrEditTransactionRequest request = CreateOrEditTransactionRequest.newBuilder()
+                .setTransaction(transactionMessage)
+                .build();
+
+        Transaction transaction = Transaction.builder()
+                .type(transactionTypeMapper.toEntity(transactionMessage.getTransactionType()))
+                .amount(transactionMessage.getAmount())
+                .date(new Date(transactionMessage.getDate() * 1000))
+                .id(transactionMessage.getId())
+                .name(transactionMessage.getName())
+                .build();
+
+        when(transactionMapper.toEntity(transactionMessage)).thenReturn(transaction);
+        when(transactionRepository.findById(id)).thenReturn(Optional.empty());
+
+        StatusRuntimeException expected = new StatusRuntimeException(Status.NOT_FOUND.withDescription(String.format("Transaction with id '%d' not found", id)));
+
+        StreamRecorder<SuccessResponse> responseObserver = StreamRecorder.create();
+
+        householdService.editTransaction(request, responseObserver);
+
+        if (!responseObserver.awaitCompletion(5, TimeUnit.SECONDS)) {
+            fail("The call did not terminate in time");
+        }
+
+        assertNotNull(responseObserver.getError());
+        assertEquals(StatusRuntimeException.class, responseObserver.getError().getClass());
+        assertEquals(expected.getStatus().getCode(), ((StatusRuntimeException) responseObserver.getError()).getStatus().getCode());
+        assertEquals(expected.getMessage(), responseObserver.getError().getMessage());
+    }
+
+    @Test
+    public void editTransaction_transactionDoesExist_SuccessResponseReturned() throws Exception {
+        long id = 0L;
+
+        TransactionMessage transactionMessage = TransactionMessage.newBuilder()
+                .setName("Income")
+                .setId(id)
+                .setDate(new Date().getTime())
+                .setAmount(100.0)
+                .setTransactionType(TransactionMessage.TransactionType.INCOME)
+                .build();
+
+        CreateOrEditTransactionRequest request = CreateOrEditTransactionRequest.newBuilder()
+                .setTransaction(transactionMessage)
+                .build();
+
+        Transaction transaction = Transaction.builder()
+                .type(transactionTypeMapper.toEntity(transactionMessage.getTransactionType()))
+                .amount(transactionMessage.getAmount())
+                .date(new Date(transactionMessage.getDate() * 1000))
+                .id(transactionMessage.getId())
+                .name(transactionMessage.getName())
+                .build();
+
+        when(transactionMapper.toEntity(transactionMessage)).thenReturn(transaction);
+        when(transactionRepository.findById(id)).thenReturn(Optional.of(transaction));
+
+        StreamRecorder<SuccessResponse> responseObserver = StreamRecorder.create();
+
+        householdService.editTransaction(request, responseObserver);
+
+        if (!responseObserver.awaitCompletion(5, TimeUnit.SECONDS)) {
+            fail("The call did not terminate in time");
+        }
+
+        assertNull(responseObserver.getError());
+        assertNotNull(responseObserver.getValues());
+        assertEquals(1, responseObserver.getValues().size());
+        verify(transactionRepository, times(1)).save(transaction);
+
+        SuccessResponse response = responseObserver.getValues().get(0);
+        assertNotNull(response);
+        assertTrue(response.getSuccess());
+    }
+
+    @Test
+    public void createTransaction_householdDoesNotExist_NotFoundStatusReturned() throws Exception {
+        long household_id = 0L;
+        long id = 0L;
+
+        TransactionMessage transactionMessage = TransactionMessage.newBuilder()
+                .setName("Income")
+                .setId(id)
+                .setDate(new Date().getTime())
+                .setAmount(100.0)
+                .setTransactionType(TransactionMessage.TransactionType.INCOME)
+                .build();
+
+        CreateOrEditTransactionRequest request = CreateOrEditTransactionRequest.newBuilder()
+                .setHouseholdId(household_id)
+                .setTransaction(transactionMessage)
+                .build();
+
+        when(householdRepository.getById(household_id)).thenReturn(null);
+
+        StatusRuntimeException expected = new StatusRuntimeException(Status.NOT_FOUND.withDescription(String.format("Household with id '%d' not found", household_id)));
+
+        StreamRecorder<SuccessResponse> responseObserver = StreamRecorder.create();
+
+        householdService.createTransaction(request, responseObserver);
+
+        if (!responseObserver.awaitCompletion(5, TimeUnit.SECONDS)) {
+            fail("The call did not terminate in time");
+        }
+
+        assertNotNull(responseObserver.getError());
+        assertEquals(StatusRuntimeException.class, responseObserver.getError().getClass());
+        assertEquals(expected.getStatus().getCode(), ((StatusRuntimeException) responseObserver.getError()).getStatus().getCode());
+        assertEquals(expected.getMessage(), responseObserver.getError().getMessage());
+    }
+
+    @Test
+    public void createTransaction_householdExists_SuccessResponseReturned() throws Exception {
+        long household_id = 0L;
+        long id = 0L;
+
+        TransactionMessage transactionMessage = TransactionMessage.newBuilder()
+                .setName("Income")
+                .setId(id)
+                .setDate(new Date().getTime())
+                .setAmount(100.0)
+                .setTransactionType(TransactionMessage.TransactionType.INCOME)
+                .build();
+
+        CreateOrEditTransactionRequest request = CreateOrEditTransactionRequest.newBuilder()
+                .setHouseholdId(household_id)
+                .setTransaction(transactionMessage)
+                .build();
+
+        Household household = Household.builder()
+                .transactions(new HashSet<>())
+                .id(household_id)
+                .owner("owner@owner.com")
+                .balance(100.0)
+                .build();
+
+        Transaction transaction = Transaction.builder()
+                .type(transactionTypeMapper.toEntity(transactionMessage.getTransactionType()))
+                .amount(transactionMessage.getAmount())
+                .date(new Date(transactionMessage.getDate() * 1000))
+                .id(transactionMessage.getId())
+                .name(transactionMessage.getName())
+                .build();
+
+        when(householdRepository.getById(household_id)).thenReturn(household);
+        when(transactionMapper.toEntity(transactionMessage)).thenReturn(transaction);
+
+        StreamRecorder<SuccessResponse> responseObserver = StreamRecorder.create();
+
+        householdService.createTransaction(request, responseObserver);
+
+        if (!responseObserver.awaitCompletion(5, TimeUnit.SECONDS)) {
+            fail("The call did not terminate in time");
+        }
+
+        assertNull(responseObserver.getError());
+        assertNotNull(responseObserver.getValues());
+        assertEquals(1, responseObserver.getValues().size());
+        verify(householdRepository, times(1)).save(household);
+
+        SuccessResponse response = responseObserver.getValues().get(0);
+        assertNotNull(response);
+        assertTrue(response.getSuccess());
+    }
+
+    @Test
+    public void deleteTransaction_householdDoesNotExist_NotFoundStatusReturned() throws Exception {
+        long id = 0L;
+
+        GetOrDeleteTransactionRequest request = GetOrDeleteTransactionRequest.newBuilder()
+                .setTransactionId(id)
+                .build();
+
+        when(householdRepository.getByTransactionId(id)).thenReturn(null);
+
+        StatusRuntimeException expected = new StatusRuntimeException(Status.NOT_FOUND.withDescription(String.format("Transaction with id '%d' not found", id)));
+
+        StreamRecorder<SuccessResponse> responseObserver = StreamRecorder.create();
+
+        householdService.deleteTransaction(request, responseObserver);
+
+        if (!responseObserver.awaitCompletion(5, TimeUnit.SECONDS)) {
+            fail("The call did not terminate in time");
+        }
+
+        assertNotNull(responseObserver.getError());
+        assertEquals(StatusRuntimeException.class, responseObserver.getError().getClass());
+        assertEquals(expected.getStatus().getCode(), ((StatusRuntimeException) responseObserver.getError()).getStatus().getCode());
+        assertEquals(expected.getMessage(), responseObserver.getError().getMessage());
+    }
+
+    @Test
+    public void deleteTransaction_transactionFound_SuccessResponseReturned() throws Exception {
+        long id = 0L;
+
+        GetOrDeleteTransactionRequest request = GetOrDeleteTransactionRequest.newBuilder()
+                .setTransactionId(id)
+                .build();
+
+        Transaction transaction = Transaction.builder()
+                .name("inc1")
+                .id(id)
+                .date(new Date())
+                .amount(300.0)
+                .type(TransactionType.INCOME)
+                .build();
+
+        Set<Transaction> transactions = new HashSet<>();
+        transactions.add(transaction);
+
+        Household household = Household.builder()
+                .balance(0.0)
+                .owner("owner@owner.com")
+                .id(0L)
+                .transactions(transactions)
+                .build();
+
+        when(householdRepository.getByTransactionId(id)).thenReturn(household);
+
+        StreamRecorder<SuccessResponse> responseObserver = StreamRecorder.create();
+
+        householdService.deleteTransaction(request, responseObserver);
+
+        if (!responseObserver.awaitCompletion(5, TimeUnit.SECONDS)) {
+            fail("The call did not terminate in time");
+        }
+
+        assertNull(responseObserver.getError());
+        assertNotNull(responseObserver.getValues());
+        assertEquals(1, responseObserver.getValues().size());
+        verify(householdRepository, times(1)).save(household);
+
+        SuccessResponse response = responseObserver.getValues().get(0);
+        assertNotNull(response);
+        assertTrue(response.getSuccess());
+    }
+
+    @Test
+    public void createHousehold_ownerAlreadyHasHousehold_AlreadyExistStatusReturned() throws Exception {
+        String owner = "owner@owner.com";
+
+        HouseholdRequest request = HouseholdRequest.newBuilder()
+                .setOwner(owner)
+                .build();
+
+        Household household = Household.builder()
+                .transactions(new HashSet<>())
+                .id(0L)
+                .owner(owner)
+                .balance(0.0)
+                .build();
+
+        when(householdRepository.getByOwner(owner)).thenReturn(household);
+
+        StatusRuntimeException expected = new StatusRuntimeException(Status.ALREADY_EXISTS.withDescription(String.format("Household for owner '%s' already exists", owner)));
+
+        StreamRecorder<SuccessResponse> responseObserver = StreamRecorder.create();
+
+        householdService.createHousehold(request, responseObserver);
+
+        if (!responseObserver.awaitCompletion(5, TimeUnit.SECONDS)) {
+            fail("The call did not terminate in time");
+        }
+
+        assertNotNull(responseObserver.getError());
+        assertEquals(StatusRuntimeException.class, responseObserver.getError().getClass());
+        assertEquals(expected.getStatus().getCode(), ((StatusRuntimeException) responseObserver.getError()).getStatus().getCode());
+        assertEquals(expected.getMessage(), responseObserver.getError().getMessage());
+    }
+
+    @Test
+    public void createHousehold_ownerDoesNotHaveHousehold_SuccessResponseReturned() throws Exception {
+        String owner = "owner@owner.com";
+
+        HouseholdRequest request = HouseholdRequest.newBuilder()
+                .setOwner(owner)
+                .build();
+
+        when(householdRepository.getByOwner(owner)).thenReturn(null);
+
+        StreamRecorder<SuccessResponse> responseObserver = StreamRecorder.create();
+
+        householdService.createHousehold(request, responseObserver);
+
+        if (!responseObserver.awaitCompletion(5, TimeUnit.SECONDS)) {
+            fail("The call did not terminate in time");
+        }
+
+        assertNull(responseObserver.getError());
+        assertNotNull(responseObserver.getValues());
+        assertEquals(1, responseObserver.getValues().size());
+        verify(householdRepository, times(1)).save(Mockito.any());
+
+        SuccessResponse response = responseObserver.getValues().get(0);
+        assertNotNull(response);
+        assertTrue(response.getSuccess());
     }
 }
